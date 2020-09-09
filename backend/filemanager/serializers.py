36f36aee05562466701fff938caf2ab917ca01ec
Joel Thanwerdas <joel.thanwerdas@gmail.com>
2020-09-09 16:17:18 +0200

    viewsets fixed

--- backend/filemanager/serializers.py
+++ backend/filemanager/serializers.py
@@ -49,7 +49,7 @@ class ExerciceSerializer(serializers.ModelSerializer):
 
     def get_filetype(self, obj):
         if obj.file:
-            filename = obj.file.name
+            filename = obj.file.namez
             return filename.split('.')[-1]
         return None
 
